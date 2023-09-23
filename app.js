541de57efb92ead78dcaf3bd4d0060aa7abc17be
ghostsheep <psupacheep@gmail.com>
2023-09-23 20:31:01 +0700

    update : headbar 0.12

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const addFood = document.querySelector("#addNewOne")
-addFood.addEventListener('click',()=>{
-    console.log("you click add one menu!!!")
+const search = document.querySelector("#search")
+const magaZine = document.querySelector("#magazine")
+const newLetter = document.querySelector("#newletter")
+const log_in = document.querySelector("#log_in")
+
+search.addEventListener('click',()=>{
+    console.log("you click search!!!");
 });
+magaZine.addEventListener('click',()=>{
+    console.log("you click magazine!!!");
+});
+newLetter.addEventListener('click',()=>{
+    console.log("you click newletter!!!");
+});
+log_in.addEventListener('click',()=>{
+    console.log("you click log_in!!!");
+});
+
+
+
+
+addFood.addEventListener('click',()=>{
+    console.log("you click add one menu!!!");
+});
+
